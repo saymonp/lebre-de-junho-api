32c5a5b0c9eae2cc3d20759cb1e3cff90f6d579e
Saymon <saymon.T@outlook.com>
2026-07-07 02:17:32 -0300

    feat: address resource

---
+++ app/Http/Resources/AddressResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AddressResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'titulo' => $this->titulo,
+            'destinatario' => $this->destinatario,
+            'telefone' => $this->telefone,
+            'cep' => $this->cep,
+            // Linha única formatada para facilitar a exibição na Dashboard/Nuxt
+            'endereco_completo' => "{$this->logradouro}, nº {$this->numero} - {$this->bairro}, {$this->cidade}/{$this->estado}",
+            'detalhes' => [
+                'logradouro' => $this->logradouro,
+                'numero' => $this->numero,
+                'bairro' => $this->bairro,
+                'cidade' => $this->cidade,
+                'estado' => $this->estado,
+                'complemento' => $this->complemento,
+            ]
+        ];
+    }
+}

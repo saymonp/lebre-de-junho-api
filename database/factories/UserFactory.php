0fa601bf2dfea471cba28451770b2489ce8912fb
Saymon <saymon.T@outlook.com>
2026-07-07 19:25:31 -0300

    feat(user factory): add lgpd terms

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,12 +24,17 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $nome = fake()->name();
         return [
-            'name' => fake()->name(),
+            'name' => $nome,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'slug' => Str::slug($nome),
+            'google_id' => Str::random(6),
+            'aceitou_termos' => true,
+            'termos_versao' => 1.0
         ];
     }
 
@@ -38,7 +43,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }

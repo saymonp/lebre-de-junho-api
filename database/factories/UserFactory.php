6c5999e04ff81cf628ff0e6846613076659bbdd7
Saymon <saymon.T@outlook.com>
2026-07-08 18:28:58 -0300

    fix: remove slug de usuário

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,14 +24,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $nome = fake()->name();
         return [
-            'name' => $nome,
+            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'slug' => Str::slug($nome),
             'google_id' => Str::random(6),
             'aceitou_termos' => true,
             'termos_versao' => 1.0

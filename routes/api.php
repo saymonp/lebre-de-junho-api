797d5dc964ffb67acb1c2466d450f18714e285ee
Saymon <saymon.T@outlook.com>
2026-07-07 23:46:48 -0300

    feat(user): api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [UserController::class, 'getProfile']);
+    Route::put('/user', [UserController::class, 'update']);
+    // Sem nenhum "{user}" na URL. O Sanctum já sabe quem é pelo Token!
+    Route::delete('/user', [UserController::class, 'deleteOwnAccount']);
+});
+
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('/users/{user}/roles', [UserController::class, 'assignRole']);
+    Route::post('/users/{user}/permissions', [UserController::class, 'assignPermissions']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+    Route::get('/roles', [UserController::class, 'listRoles']);
+    Route::get('/users', [UserController::class, 'index']);
+});
+
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/verify/email', [UserController::class, 'verifyEmail']);
+Route::post('/auth/google/callback', [UserController::class, 'handleGoogleCallback']);
+Route::post('/recover-password', [UserController::class, 'recoverPassword']);
+Route::post('/reset-password', [UserController::class, 'resetPassword']);

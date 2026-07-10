d40a5b0c8c55ab28a782a0fb25021137daa9e7ea
Saymon <saymon.T@outlook.com>
2026-07-10 14:27:38 -0300

    chore(config): chaves do google

--- config/services.php
+++ config/services.php
@@ -35,4 +35,12 @@ return [
         ],
     ],
 
+    'google' => [
+        'translation_key' => env('GOOGLE_API_KEY'),
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+        'redirect_frontend' => env('GOOGLE_REDIRECT_FRONTEND'),
+    ],
+
 ];

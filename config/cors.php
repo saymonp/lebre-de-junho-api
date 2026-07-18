80c362c454e60dee50749688c36f7ce288bfbbc8
Saymon <saymon.T@outlook.com>
2026-07-18 17:19:53 -0300

    chore: cors config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], // Em produção, troque '*' pela URL definitiva
+    'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000')),
 
     'allowed_origins_patterns' => [],
 
